cee7dd1945e6614d682194020a2ee46ccc6e9674
Edmilson Expedito <edmilson.expedito@objective.com.br>
2022-09-27 17:56:18 -0300

    Separando o main do terraform

---
+++ ecr.tf
@@ -0,0 +1,3 @@
+resource "aws_ecr_repository" "ecr_repo_python" {
+  name = "nb2b-rds-kinesis-dev-repo" # Naming my repository
+}
---
+++ ecs_task.tf
@@ -0,0 +1,25 @@
+resource "aws_ecs_task_definition" "task_python" {
+  family                   = "my-first-task" # Naming our first task
+  container_definitions    = <<DEFINITION
+  [
+    {
+      "name": "my-first-task",
+      "image": "${aws_ecr_repository.ecr_repo_python.repository_url}",
+      "essential": true,
+      "portMappings": [
+        {
+          "containerPort": 80,
+          "hostPort": 80
+        }
+      ],
+      "memory": 512,
+      "cpu": 256
+    }
+  ]
+  DEFINITION
+  requires_compatibilities = ["FARGATE"] # Stating that we are using ECS Fargate
+  network_mode             = "awsvpc"    # Using awsvpc as our network mode as this is required for Fargate
+  memory                   = 512         # Specifying the memory our container requires
+  cpu                      = 256         # Specifying the CPU our container requires
+  execution_role_arn       = "${aws_iam_role.ecsTaskExecutionRole.arn}"
+}
